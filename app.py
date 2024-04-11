e0b9c5fefcd295e1ae51eb38b88bd40ba5c93df7
wobnets <9gy43end@anonaddy.me>
2024-04-11 17:38:13 -0400

    all in memory

--- app.py
+++ app.py
@@ -1,15 +1,38 @@
 from fastapi import FastAPI, Form, UploadFile, File
 from typing import Optional, Annotated
-from send_email import send_email_via_mailgun
 import os
+import requests
+from dotenv import load_dotenv
 
+load_dotenv()
 
-api_key = os.environ.get("MAILGUN_API_KEY")
-domain = os.environ.get("DOMAIN")
-from_addr = f"{os.environ.get('FROM_USR')}@{domain}"
-to_addr = os.environ.get("TO_ADDR")
+API_KEY = os.environ.get("MAILGUN_API_KEY")
+DOMAIN = os.environ.get("DOMAIN")
+FROM_ADDR = f"{os.environ.get('FROM_USR')}@{DOMAIN}"
+TO_ADDR = os.environ.get("TO_ADDR")
 
 app = FastAPI()
+
+def send_email_via_mailgun(subject, text, filename, file_content):
+    url = f"https://api.mailgun.net/v3/{DOMAIN}/messages"
+
+    auth=("api", f"{API_KEY}")
+
+    files=[("attachment", (filename, file_content))]
+
+    data = {
+        "from": FROM_ADDR,
+        "to": TO_ADDR,
+        "subject": subject,
+        "text": text,
+    }
+
+    response = requests.post(url, auth=auth, data=data, files=files)
+
+    if response.status_code == 200:
+        print("Email sent successfully!")
+    else:
+        print(f"Failed to send email. Status code: {response.status_code}, Response: {response.text}")
 
 
 @app.post("/submit-form/")
@@ -20,12 +43,9 @@ async def handle_form_submission(
     resume: Annotated[UploadFile, File()],
     message: Optional[Annotated[str, Form()]] = None,
 ):
-    file_location = f"./{resume.filename}"
+    file_contents = await resume.read()
 
-    with open(file_location, "wb") as file_object:
-        file_object.write(resume.file.read())
-
-    # Send an email with the form data
+    # send email
     subject = f"New Employment Application from {name}"
     text = f"""
     Name: {name}
@@ -34,9 +54,9 @@ async def handle_form_submission(
     Message: {message}
     """
 
-    send_email_via_mailgun(api_key, domain, from_addr, to_addr, subject, text, file_location)
-
-    # remove the file after sending the email
-    os.remove(file_location)
+    send_email_via_mailgun(subject, text, resume.filename, file_contents)
 
     return "Form submitted successfully! We will get back to you soon."
+
+if __name__ == "__main__":
+    pass
